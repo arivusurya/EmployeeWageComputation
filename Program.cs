fb8e909ddbc88b21bc9f57c1a067686fcb485ea8
arivusurya <arivusurya.3@gmail.com>
2023-02-09 08:20:12 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeWage{
+
+
+    class Program{
+
+    public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the employe computation ");
+    }
+}
+}
